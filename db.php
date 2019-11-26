1d21c2a7b7b782808fde8a79efd94f4769b96256
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-26 15:49:55 +0000

    Created class for database

--- db.php
+++ db.php
@@ -1,4 +1,17 @@
 <?php
 
+class DB
+{
+
+    private $host           = '127.0.0.1';
+    private $port           = '3306';
+    private $db             = 'social_media';
+    private $username       = 'root';
+    private $password       =  null;
+    private $charset        = 'utf8mb4';
+    private $dsn;
+
+
+}
 
 ?>
