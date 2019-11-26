3e04e186a1eb784b401179bf2983350b5b0fc0ec
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-26 15:52:27 +0000

    Added paths to other web pages

--- index.php
+++ index.php
@@ -9,4 +9,20 @@ $_SESSION;
 $user = false;
 $created = false;
 
+$logged_in = $_SESSION['logged_in'] ?? false;
+
+if(!$logged_in)
+{
+    $created = false;
+    include 'login_form.html';
+}
+else if($logged_in)
+{
+    include 'profile.php';
+}
+else if(!$created)
+{
+    include 'create_account.html';
+}
+
 ?>
