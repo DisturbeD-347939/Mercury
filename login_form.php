5d183ed1ddff2869aacf524abe955e135d5a9ecf
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-26 17:34:04 +0000

    Create login_form.php

---
+++ login_form.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'db.php';
+
+$db = new DB;
+
+if(!session_id())
+{
+    @session_start();
+}
+
+if(!empty($_POST))
+{
+    $result = $db->login($_POST['log_in']['email'], hash('sha256', $_POST['log_in']['password']));
+    if($result[0])
+    {
+        $_SESSION['login'] = true;
+    }
+    else
+    {
+        echo "Incorrect details, try again!";
+        include 'login_form.html';
+    }
+}
+else
+{
+    include 'login_form.html';
+}
+
+?>
