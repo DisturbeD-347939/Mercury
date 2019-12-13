b1870194161aebaede1c98fdea78926032f9ff57
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-12-13 00:11:28 +0000

    Functions to get posts from the user's follow list

--- js/feed.js
+++ js/feed.js
@@ -1,11 +1,61 @@
-setTimeout(setup, 500);
+setTimeout(run, 50);
 
-function setup()
+//HEEEEEEEEEEEEEEEEEEEEEEEEEELP
+function request(ids, callback)
+{
+    $.ajax
+    ({
+        type: 'GET',
+        url: 'requests.php',
+        contentType: 'application/json; charset=utf-8',
+        data: {"getPosts":ids},
+        dataType: 'json',
+        success: function (response)
+        {
+            callback(response);
+        },
+        error: function ()
+        {
+            callback("fail");
+        } 
+    });
+}
+
+function run()
 {
     ids = JSON.parse(ids);
-    console.log(ids);
+    parseIDs(ids, function(data)
+    {
+        request(data, function(data)
+        {
+            for(var i = 0; i < data["result"][0].length; i++)
+            {
+                console.log(data["result"][0][i]);
+                $('#feedPosts').append("<div id=" + data["result"][0][i]["id"] + "><hr><h3>" + data["result"][0][i]["title"] + "</h3><p>" + data["result"][0][i]["content"] + "</p></div>");
+            }
+        })
+    })
+    
+}
+
+function parseIDs(ids, callback)
+{
+    var data = "";
     for(var i = 0; i < ids.length; i++)
     {
-        console.log(ids[i]["userID"]);
+        if(i+1 == ids.length)
+        {
+            data += ids[i]["userID"];
+        }
+        else
+        {
+            data += ids[i]["userID"] + ",";
+        }
     }
+    callback(data);
+}
+
+function displayPosts()
+{
+    console.log("Displaying");
 }
