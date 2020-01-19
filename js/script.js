e68a4d078ad8283ca00e239873d4dc3ed233831e
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-19 16:34:43 +0000

    Fixed posts on profile page

--- js/script.js
+++ js/script.js
@@ -10,23 +10,37 @@ function displayPosts()
         posts[1] = posts[1].reverse();
         //Empty any posts that might be there already
         $("#posts").empty();
-        for(var i = 0; i < posts[1].length; i++)
-        {
-            //Turn timestamp into milliseconds
-            var date = new Date(posts[1][i].timestamp * 1000);
-            //Set date to when post was created
-            date = date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "." + date.getMonth()+1 + "." + date.getFullYear();
 
-            //Check if it's the users profile or another user profile
-            if(!searching)
+        request(posts[1][0].userID, function(data)
+        {
+            var tempArray = [];
+            var timestamps = [];
+
+            for(var i = 0; i < data[0]["result"][0].length; i++)
             {
-                $('#posts').append("<div id=" + posts[1][i].id + "><hr><h3>" + posts[1][i].title + "</h3><p>" + posts[1][i].content + "</p><p><small>" + date + "</small></p><button onclick='deletePost(" + posts[1][i].id + ")'>Delete post</button></div>");
+                tempArray.push(data[0]["result"][0][i]["timestamp"]);
+
+                //Turn timestamp into milliseconds
+                var date = new Date(data[0]["result"][0][i]["timestamp"] * 1000);
+                //Set date to when post was created
+                date = date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "." + date.getMonth()+1 + "." +date.getFullYear();
+                //Get names
+
+                checkLikes(data[0]["result"][0][i]["id"]);
+                
+                tempArray.push("<div class='post userPost' id=" + data[0]["result"][0][i]["id"] + "><img src='" + data[2]["result"][0][1] + "'></img><h2>" +  data[1]["result"][0][0]["first_name"] + " " + data[1]["result"][0][0]["surname"] + "</h2><h3>" +data[0]["result"][0][i]["title"] + "</h3><p id='content'>" + data[0]["result"][0][i]["content"] + "</p><p><small>" + date + "</small></p><div onclick=like(" + data[0]["result"][0][i]["id"] + ")><img id='postLike' src='../images/dislike.png'></img><p>0</p></div><button onclick='deletePost(" + data[0]["result"][0][i]["id"] + ")'></button></div>");
+
+                timestamps.push(tempArray);
+                tempArray = [];
             }
-            else
+            timestamps.reverse(timestamps.sort(sortFunction));
+            for(var i = 0; i < timestamps.length; i++)
             {
-                $('#posts').append("<div id=" + posts[1][i].id + "><hr><h3>" + posts[1][i].title + "</h3><p>" + posts[1][i].content + "</p><p><small>" + date + "</small></p></div>");
+                $('#posts').append(timestamps[i][1]);
             }
-        }
+        });
+
+        
     }
 }
 
