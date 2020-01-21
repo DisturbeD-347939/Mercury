1780c3910aa41de2a6682846b3d0c5d028cc295b
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-01-21 21:45:55 +0000

    Added tags functionality

--- js/script.js
+++ js/script.js
@@ -4,54 +4,58 @@ setTimeout(displayPosts, 50);
 function displayPosts()
 {
     //If there are posts to display
-    if(posts.length != 0 && posts[0] != 0)
+    if(posts != null)
     {
-        //Reverse the order from newest to oldest
-        posts[1] = posts[1].reverse();
-        //Empty any posts that might be there already
-        $("#posts").empty();
-
-        request(posts[1][0].userID, function(data)
+        console.log(posts);
+        if(posts.length != 0 && posts[0] != 0 && posts.length != null)
         {
-            var tempArray = [];
-            var timestamps = [];
-
-            for(var i = 0; i < data[0]["result"][0].length; i++)
+            //Reverse the order from newest to oldest
+            posts[1] = posts[1].reverse();
+            //Empty any posts that might be there already
+            $("#posts").empty();
+    
+            request(posts[1][0].userID, function(data)
             {
-                tempArray.push(data[0]["result"][0][i]["timestamp"]);
-
-                //Turn timestamp into milliseconds
-                var date = new Date(data[0]["result"][0][i]["timestamp"] * 1000);
-                //Set date to when post was created
-                date = date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "." + date.getMonth()+1 + "." +date.getFullYear();
-                //Get names
-
-                checkLikes(data[0]["result"][0][i]["id"]);
-                checkPhotos(data[0]["result"][0][i]["id"]);
-                checkComments(data[0]["result"][0][i]["id"]);
-                
-                
-                if(!searching)
+                var tempArray = [];
+                var timestamps = [];
+    
+                for(var i = 0; i < data[0]["result"][0].length; i++)
                 {
-                    tempArray.push("<div class='post userPost' id=" + data[0]["result"][0][i]["id"] + "><img class='profilePic' src='" + data[2]["result"][0][1] + "'></img><h2>" +  data[1]["result"][0][0]["first_name"] + " " + data[1]["result"][0][0]["surname"] + "</h2><h3>" +data[0]["result"][0][i]["title"] + "</h3><p class='content'>" + data[0]["result"][0][i]["content"] + "</p><p class='date'><small>" + date + "</small></p><img class='postPic'><div class='like postButtons' onclick=like(" + data[0]["result"][0][i]["id"] + ")></img><img class='postLike' src='../images/dislike.png'></img><p>0</p></div><div class='comments postButtons' onclick=showComments(" + data[0]["result"][0][i]["id"] + ")><img class='hiddenComments' src='../images/hiddenComments.png'></img><p>0</p></div><button onclick='deletePost(" + data[0]["result"][0][i]["id"] + ")'></button><div class='commentBox'></div></div>");
+                    tempArray.push(data[0]["result"][0][i]["timestamp"]);
+    
+                    //Turn timestamp into milliseconds
+                    var date = new Date(data[0]["result"][0][i]["timestamp"] * 1000);
+                    //Set date to when post was created
+                    date = date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "." + date.getMonth()+1 + "." +date.getFullYear();
+                    //Get names
+    
+                    checkLikes(data[0]["result"][0][i]["id"]);
+                    checkPhotos(data[0]["result"][0][i]["id"]);
+                    checkComments(data[0]["result"][0][i]["id"]);
+                    
+                    
+                    if(!searching)
+                    {
+                        tempArray.push("<div class='post userPost' id=" + data[0]["result"][0][i]["id"] + "><img class='profilePic' src='" + data[2]["result"][0][1] + "'></img><h2>" +  data[1]["result"][0][0]["first_name"] + " " + data[1]["result"][0][0]["surname"] + "</h2><h3>" +data[0]["result"][0][i]["title"] + "</h3><p class='content'>" + data[0]["result"][0][i]["content"] + "</p><p class='date'><small>" + date + "</small></p><img class='postPic'><div class='like postButtons' onclick=like(" + data[0]["result"][0][i]["id"] + ")></img><img class='postLike' src='../images/dislike.png'></img><p>0</p></div><div class='comments postButtons' onclick=showComments(" + data[0]["result"][0][i]["id"] + ")><img class='hiddenComments' src='../images/hiddenComments.png'></img><p>0</p></div><button onclick='deletePost(" + data[0]["result"][0][i]["id"] + ")'></button><div class='commentBox'></div></div>");
+                    }
+                    else
+                    {
+                        tempArray.push("<div class='post userPost' id=" + data[0]["result"][0][i]["id"] + "><img class='profilePic' src='" + data[2]["result"][0][1] + "'></img><h2>" +  data[1]["result"][0][0]["first_name"] + " " + data[1]["result"][0][0]["surname"] + "</h2><h3>" +data[0]["result"][0][i]["title"] + "</h3><p class='content'>" + data[0]["result"][0][i]["content"] + "</p><p class='date'><small>" + date + "</small></p><div class='like postButtons' onclick=like(" + data[0]["result"][0][i]["id"] + ")><img class='postLike' src='../images/dislike.png'></img><p>0</p></div><div class='comments postButtons' onclick=showComments(" + data[0]["result"][0][i]["id"] + ")><img class='hiddenComments' src='../images/hiddenComments.png'></img><p>0</p></div><div class='commentBox'></div></div>");
+                    }
+    
+                    timestamps.push(tempArray);
+                    tempArray = [];
                 }
-                else
+                timestamps.reverse(timestamps.sort(sortFunction));
+                for(var i = 0; i < timestamps.length; i++)
                 {
-                    tempArray.push("<div class='post userPost' id=" + data[0]["result"][0][i]["id"] + "><img class='profilePic' src='" + data[2]["result"][0][1] + "'></img><h2>" +  data[1]["result"][0][0]["first_name"] + " " + data[1]["result"][0][0]["surname"] + "</h2><h3>" +data[0]["result"][0][i]["title"] + "</h3><p class='content'>" + data[0]["result"][0][i]["content"] + "</p><p class='date'><small>" + date + "</small></p><div class='like postButtons' onclick=like(" + data[0]["result"][0][i]["id"] + ")><img class='postLike' src='../images/dislike.png'></img><p>0</p></div><div class='comments postButtons' onclick=showComments(" + data[0]["result"][0][i]["id"] + ")><img class='hiddenComments' src='../images/hiddenComments.png'></img><p>0</p></div><div class='commentBox'></div></div>");
+                    $('#posts').append(timestamps[i][1]);
+                    hashtagIt('.userPost');
                 }
-
-                timestamps.push(tempArray);
-                tempArray = [];
-            }
-            timestamps.reverse(timestamps.sort(sortFunction));
-            for(var i = 0; i < timestamps.length; i++)
-            {
-                $('#posts').append(timestamps[i][1]);
-            }
-        });
-
-        
+            });
+        }
     }
+    
 }
 
 //Create new post AJAX request
